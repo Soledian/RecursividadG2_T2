e438e7e9089cb3bcbe8d1213d6fcba4896f8bffc
diana evelin lozano <dianaevelinlozano4@gmail.com>
2026-09-09 14:21:55 -0600

    Version_2.0

---
+++ Rcurcividad.Logica/Recursividades.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rcurcividad.Logica
+{
+    public  class Recursividades
+    {
+
+        public int CalcularFactorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            return num * CalcularFactorial(num - 1);
+
+        }
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,9 @@
+﻿using Rcurcividad.Logica;
+
+Console.WriteLine("Metodos recurcivos");
+Console.WriteLine("\n\n Calcular factorial de 5");
+
+Recursividades oRecursividades = new Recursividades();
+int resultado = oRecursividades.CalcularFactorial(5);
+
+Console.WriteLine("el resultado es:" + resultado);
